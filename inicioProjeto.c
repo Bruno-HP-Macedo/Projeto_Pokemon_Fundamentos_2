88243c1b0dcdd80fe8c14c994762d0c0d8b531df
Jonathan Dahlin <jonathandahlin.bgs@gmail.com>
2023-11-22 18:24:41 -0300

    inicio em aula

---
+++ inicioProjeto.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+//Pokemanos Inicio do projeto//
+
+struct Pokemon{
+int numero;
+char nome[50];
+char tipo1[20];
+char tipo2[20];
+int total;
+int atk;
+int def;
+int atk_Especial;
+int def_Especial;
+int Velocidade;
+int Geracao;
+bool Lendario;
+char cor [50];
+float altura;
+float peso;
+int taxaCaptura;
+int proxEvolucao;
+int pre_Evolucao;
+};//Estrutura Pokemon
+
+struct Colecao{
+
+
+};//Estrutura coleção
+
+struct Mochila{
+
+
+};//Estrutura Mochila
+
+
+
+int main(){
+
+
+    return 0;
+}
